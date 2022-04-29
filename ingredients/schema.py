678940ba9677697a136db481bcd50e1a9b43cf11
David Oluwafemi Joshua <davidoluwafemi178@gmail.com>
2022-04-29 11:55:05 +0100

    register user

--- ingredients/schema.py
+++ ingredients/schema.py
@@ -10,7 +10,7 @@ from .models import Category, Ingredient
 class UserType(DjangoObjectType):
     class Meta:
         model = User
-        fields = ("username", "email")
+        fields = ("username",)
 
 
 class CategoryType(DjangoObjectType):
@@ -27,7 +27,8 @@ class IngredietType(DjangoObjectType):
 
 class Query(graphene.ObjectType):
     viewer = graphene.Field(UserType, token=graphene.String(required=True))
-    all_ingredients = graphene.List(IngredietType
+    all_ingredients = graphene.List(IngredietType)
+    all_categories = graphene.List(CategoryType)
     category_by_name = graphene.Field(
         CategoryType,
         name=graphene.String(required=True),
@@ -39,11 +40,35 @@ class Query(graphene.ObjectType):
 
     def resolve_all_ingredients(self, info):
         return Ingredient.objects.select_related("category").all()
+
     def resolve_category_by_name(self, info, name):
         try:
             return Category.objects.get(name=name.capitalize())
         except Category.DoesNotExist:
             return None
+
+    def resolve_all_categories(self, info):
+        return Category.objects.all()
+
+
+class CreateUser(graphene.Mutation):
+    message = graphene.String()
+    user = graphene.Field(UserType)
+
+    class Arguments:
+        username = graphene.String(required=True)
+        password = graphene.String(required=True)
+        email = graphene.String()
+
+    def mutate(self, info, **kwargs):
+        user = User.objects.create_user(
+            username=kwargs.get("username"), email=kwargs.get("email")
+        )
+        user.set_password(kwargs.get("password"))
+        user.save()
+        return CreateUser(
+            user=user, message=f"Successfully created user {user.username}"
+        )
 
 
 class Mutation(graphene.ObjectType):
@@ -51,6 +76,7 @@ class Mutation(graphene.ObjectType):
     verify_token = graphql_jwt.Verify.Field()
     refresh_token = graphql_jwt.Refresh.Field()
     revoke_token = graphql_jwt.Revoke.Field()
+    create_user = CreateUser.Field()
 
 
 schema = graphene.Schema(mutation=Mutation, query=Query)
